b0156ef5210af404b5d5da8c5cd9b15d160bf0ce
chopadeyash-git <your-email@example.com>
2026-02-22 00:37:49 +0530

    Update ShareAndLearn with latest code

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender = async (email, title, body) => {
         });
 
         const info = await transporter.sendMail({
-            from: 'StudyNotion || by Aniruddha Gade',
+            from: 'Share And Learn || By Yash Productions',
             to: email,
             subject: title,
             html: body
--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -180,21 +180,21 @@ const Footer = () => {
 
           <div className="text-center flex flex-col sm:flex-row ">
             <div className="flex ">
-              <span> Made with ❤️</span>
+              <span> Created By:-</span>
 
-              <Link to='https://github.com/Aniruddha-Gade' target="__blank" className="text-white hover:underline mr-1"
+              <Link to='' target="__blank" className="text-white hover:underline mr-1"
               >
-                Aniruddha Gade
+                Yash Chopade
               </Link>
             </div>
-            <span> © 2023 ShareAndLearn</span>
+            <span> © 2026 ShareAndLearn</span>
           </div>
 
           <div className="flex items-center">
-            <a href="https://www.linkedin.com/in/aniruddha-gade-a48800231/" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/yashgchopade/" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImLinkedin2 size={17} />
             </a>
-            <a href="https://www.github.com/Aniruddha-Gade" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/chopadeyash-git" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImGithub size={17} />
             </a>
           </div>
